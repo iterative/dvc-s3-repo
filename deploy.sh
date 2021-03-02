8c02f61c70efab38bdfaf47976702589ef9614d7
Ruslan Kuprieiev <kupruser@gmail.com>
2021-03-02 22:24:10 +0200

    deploy: add an option to build specific targets

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,8 @@ function deploy {
     docker run -e GPG_ITERATIVE_ASC -e GPG_ITERATIVE_PASS -v ~/.aws:/root/.aws -v $(pwd):/dvc -w /dvc/$2 --rm -t dvc ./upload.sh
 }
 
-deploy ubuntu deb
-deploy fedora rpm
+if [[ -z "$@" ]]; then
+    deploy ubuntu deb
+    deploy fedora rpm
+else
+    deploy $@
