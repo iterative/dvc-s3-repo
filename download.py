fd586f572f973d04c68c5dfbac4bbd67367f7a09
efiop <efiop@users.noreply.github.com>
2021-10-14 00:00:44 +0000

    dvc 2.8.1

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ import tarfile
 
 import wget
 
-VERSION = "2.7.4"
+VERSION = "2.8.1"
 URL = f"https://github.com/iterative/dvc/archive/{VERSION}.tar.gz"
 
 path = pathlib.Path(__file__).parent.absolute()
