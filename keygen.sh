01364c7ee4254758b6711e1051afab58fd6ae2ba
Ruslan Kuprieiev <kupruser@gmail.com>
2023-03-02 19:21:15 +0200

    pkg: generate new key

---
+++ keygen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+PASSWD=$(gpg --gen-random --armor 1 14)
+export GNUPGHOME="$(mktemp -d)"
+
+gpg --passphrase "$PASSWD" \
+    --pinentry-mode loopback \
+    --quick-generate-key "Iterative <eng@iterative.ai>" rsa4096 cert 2y
+
+gpg --list-keys
+
+gpg --armor --export Iterative > iterative.asc
+
+gpg --armor \
+    --passphrase "$PASSWD" \
+    --pinentry-mode loopback \
+    --export-secret-keys Iterative
+
+echo "Save secret key ^ and its passphrase $PASSWD in a secure location"
