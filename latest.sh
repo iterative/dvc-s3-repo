3ba4af96405aae2c12ae20c228f4d07238a1bb58
Ruslan Kuprieiev <kupruser@gmail.com>
2023-05-10 03:16:59 +0300

    latest: use gh CLI

--- latest.sh
+++ latest.sh
@@ -3,5 +3,4 @@
 set -e
 
 PROJECT="iterative/dvc"
-GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
-echo $(curl --silent $GHAPI_URL | jq -r .tag_name)
+echo $(gh release list -R $PROJECT -L1 | awk -F '\t' '{ print $3 }')
