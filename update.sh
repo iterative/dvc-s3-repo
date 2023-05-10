9b01c277056bc6081902b783f32293d042ba938b
Ruslan Kuprieiev <kupruser@gmail.com>
2023-05-10 03:19:25 +0300

    update: remove unused code

--- update.sh
+++ update.sh
@@ -8,8 +8,4 @@ if [ ! $LATEST ]; then
   LATEST=$(./latest.sh)
 fi
 
-PROJECT="iterative/dvc"
-GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
-LATEST=$(curl --silent $GHAPI_URL | jq -r .tag_name)
-
 sed -i 's/^VERSION = .*$/VERSION = '"\"$LATEST\""'/g' download.py
