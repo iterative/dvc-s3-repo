792970061bb1d3e118b8b28a852bb2fe6ca31cb3
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-08-05 08:29:47 +0545

    debug

--- build_bin.py
+++ build_bin.py
@@ -1,6 +1,6 @@
 import os
 import pathlib
-from subprocess import STDOUT, check_call, check_output
+from subprocess import STDOUT, CalledProcessError, check_call, check_output
 
 path = pathlib.Path(__file__).parent.absolute()
 dvc = path / "dvc" / "dvc"
@@ -18,13 +18,18 @@ check_call(
     stderr=STDOUT,
 )
 
-out = check_output(
-    [
-        path / "dist" / "dvc" / "dvc",
-        "doctor",
-    ],
-    stderr=STDOUT,
-).decode()
+try:
+    out = check_output(
+        [
+            path / "dist" / "dvc" / "dvc",
+            "doctor",
+        ],
+        stderr=STDOUT,
+    ).decode()
+except CalledProcessError as exc:
+    out = exc.output.decode()
+    print(out)
+    raise
 
 remotes = [
     "s3",
