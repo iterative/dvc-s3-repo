8df80a563f5a4957d7cc4fe9c9e2b8f16ec3b859
Helio Machado <0x2b3bfa0+git@googlemail.com>
2025-04-07 08:53:41 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -9,5 +9,5 @@ RPM_PKG="dvc-$VERSION-1.x86_64.rpm"
 DEB_PKG="dvc_""$VERSION""_amd64.deb"
 
 function upload_file {
-  aws s3 cp $1 s3://$AWS_S3_BUCKET/$2/ --acl public-read
+  aws s3 cp $1 s3://$AWS_S3_BUCKET/$2/
 }
