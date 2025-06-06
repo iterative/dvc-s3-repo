3e458486f741c4090ac5c559e0569324122923a1
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-06-06 08:52:31 +0545

    try to pass the keyid

--- deb/upload.sh
+++ deb/upload.sh
@@ -20,13 +20,15 @@ gpg --no-tty --batch --passphrase $GPG_ITERATIVE_PASS --pinentry-mode loopback -
 
 gpg --list-keys
 
+KEYID=$(gpg --list-secret-keys --with-colons | awk -F: '/^fpr:/ { print $10; exit }')
+
 deb-s3 upload --bucket $AWS_S3_BUCKET \
   --prefix $AWS_S3_PREFIX \
   --preserve-versions \
   --arch amd64 \
   --codename stable \
   --visibility bucket_owner \
-  --sign \
+  --sign "${KEYID}" \
   --gpg-options="--no-tty --batch --passphrase $GPG_ITERATIVE_PASS  --pinentry-mode loopback" \
   --access-key-id $(printenv AWS_ACCESS_KEY_ID) \
   --secret-access-key $(printenv AWS_SECRET_ACCESS_KEY) \
